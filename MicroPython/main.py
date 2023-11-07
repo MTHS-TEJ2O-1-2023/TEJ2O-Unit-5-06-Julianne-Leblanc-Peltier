3166165db88667d48ce7c65f499ba9a98ad3523a
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-11-07 18:22:57 +0000

    idk whats wrong with it doesn't work import error

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,21 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from hcsr04 import HCSR04
 
+# variables
+distance_to_object = 0
+distance_to_object = HCSR04(trigger_pin=16, echo_pin=0)
+distance_to_object = sensor.distance_cm()
 
-display.scroll("Hello, World!")
+# clean up
+display.clear()
+display.show(Image.HAPPY)
+
+# find distance from sonar
+while True:
+    display.clear()
+    distance_to_object = sensor.distance_cm()
+    if button_a.is_pressed():
+        print(distance_to_object, "cm.")
+        display.show(Image.HAPPY)
